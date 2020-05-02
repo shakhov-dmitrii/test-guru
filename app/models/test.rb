c4cbd7428f7ae398ba443a9f2c743a95fb447e16
Dmitrii Shakhov <shakhov.dmitry1@yandex.ru>
2020-05-02 21:08:33 +0300

    Complete lesson 5 tasks

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,13 @@
 class Test < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :category
+  
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+
   def self.get_titles(category_name)
-    Test.joins('join categories on categories.id = tests.categories_id')
+    Test.joins('join categories on categories.id = tests.category_id')
     .where('categories.title = ?', category_name).order(title: :desc).pluck(:title)
   end
 end
