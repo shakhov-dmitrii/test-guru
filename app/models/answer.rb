7c614e509291b99d971788a2f334f294d9caa757
Dmitrii Shakhov <shakhov.dmitry1@yandex.ru>
2020-05-03 12:43:41 +0300

    Added validation in models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
+  validates :body, presence: true
+  validate :validate_answers_in_question
+
   scope :correct_answers, -> { where(correct: true) }
+
+  private
+  def validate_answers_in_question
+    errors.add(:question) if self.question.answers.count >= 4 
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,11 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :level, uniqueness: { scope: :title, message: "should happen once per title" }
+
   scope :easy_tests, -> { where(level: 0..1) }
   scope :normal_tests, -> { where(level: 2..4) }
   scope :hard_tests, -> { where(level: 5..Float::INFINITY) }
