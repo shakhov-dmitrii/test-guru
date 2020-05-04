38f9a8e94ab7166d7d180eac44c4154d07f4d167
Dmitrii Shakhov <shakhov.dmitry1@yandex.ru>
2020-05-04 14:49:39 +0300

    Fixes after review

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: [:index, :new, :create]
+  before_action :find_question, only: [:show, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -10,21 +11,22 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
-
     render inline: "<h1><%= @question.body %></h1>"
   end
 
   def new; end
 
   def create
-    @question = Question.create(question_params)
-    
-    redirect_to test_questions_path(@test)
+    @question = @test.questions.create(question_params)
+
+    if @question.errors.empty?
+      redirect_to test_questions_path(@test)
+    else
+      render inline: '<p style="color: red; font-weight: 700; font-size: 40px;"><%= @question.errors.full_messages %></p>'
+    end   
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
 
     redirect_to tests_path
@@ -32,11 +34,15 @@ class QuestionsController < ApplicationController
 
   private
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
