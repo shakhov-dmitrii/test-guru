1e18e2dc406fe2283c4e653be2c81cbc550672ef
Dmitrii Shakhov <shakhov.dmitry1@yandex.ru>
2020-05-16 22:46:54 +0300

    Fixes after review

--- app/javascript/utilities/passwordMatcher.js
+++ app/javascript/utilities/passwordMatcher.js
@@ -1,15 +1,24 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('#user_password_confirmation');
+  var control1 = document.querySelector('#user_password');
+  var control2 = document.querySelector('#user_password_confirmation');
 
-  if (control) { control.addEventListener('keyup', match); }
+  if (control1 && control2) { 
+    control1.addEventListener('keyup', match); 
+    control2.addEventListener('keyup', match); 
+  }
 })
 
 function match() {
   var password = document.querySelector('#user_password');
   var confirmationPassword  = document.querySelector('#user_password_confirmation');
 
+  if (password.value === "" || confirmationPassword.value === "") {
+    document.querySelector('.octicon-x').classList.add('hide');
+    return;
+  } 
+  
   if (password.value === confirmationPassword.value ) {
-    document.querySelector('.octicon-x').classList.add('hide')
+    document.querySelector('.octicon-x').classList.add('hide');
   } else {
     document.querySelector('.octicon-x').classList.remove('hide');
   }
